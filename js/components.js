476b12a0c272b92c22a2c47ceee3e9ba5b825d8e
Mark Pearl <markpearl@gmail.com>
2016-06-04 16:44:47 +0200

    More

--- js/components.js
+++ js/components.js
@@ -4,6 +4,18 @@
 var TimerDisplay = React.createClass({
 	render: function() {
 		return (
+			<div id='timer'>00:00</div>
+			<div id="actions">
+				<button id="runButton" className="actionButton" disabled="disabled">Start</button>
+				<button id="stopButton" className="actionButton" disabled="disabled">Stop</button>
+			 </div>
+		);
+	},	
+});
+
+var TimerDisplay2 = React.createClass({
+	render: function() {
+		return (
 			<div id='container' className='init'>
 				<div id='timer'>00:00</div>
 				<div id="actions">
@@ -15,6 +27,16 @@ var TimerDisplay = React.createClass({
 	},	
 });
 
+var MainComponent = React.createClass({
+	render: function() {
+		return (
+			<div id='container' className='init'>
+			</div>
+		);
+	},	
+});
 
 var mainElement = document.querySelector("main");
-ReactDOM.render(<TimerDisplay />, mainElement);
+ReactDOM.render(
+	<MainComponent />
+, mainElement);
