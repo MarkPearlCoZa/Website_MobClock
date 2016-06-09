b1313a82c1a6d3f1159285e6de84e75bd2499e34
Mark Pearl <markpearl@gmail.com>
2016-06-09 20:35:42 +0200

    More

--- make.sh
+++ make.sh
@@ -3,3 +3,8 @@ mkdir build
 cp -r ./src/* ./build
 rm -rfd build/js
 babel src/js -d build/js
+
+git add :/
+git commit -m "More"
+git subtree push --prefix build origin gh-pages-temp
+git push origin
