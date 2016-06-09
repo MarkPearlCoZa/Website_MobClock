1eb475998cd56b20034addbfd93f087f43165c92
Mark Pearl <markpearl@gmail.com>
2016-06-09 20:31:53 +0200

    adjust make to copy everything from src to build

---
+++ build/js/timer.js
@@ -0,0 +1,57 @@
+define(['states'], function (states) {
+
+    return function () {
+        var _elapsed = 0;
+        var _state = states.STOPPED;
+        var _interval;
+        var _callBack;
+
+        function padZero(unit) {
+            return unit < 10 ? '0' + unit : unit;
+        }
+
+        function ceaseIntervalWithStatus(interval, state) {
+            clearInterval(interval);
+            _state = state;
+        }
+
+        return {
+            start: function (callBack) {
+                if (callBack && typeof callBack === 'function') {
+                    _callBack = callBack;
+                }
+                if (_state === states.STOPPED || _state === states.PAUSED) {
+                    _interval = setInterval(function () {
+                        _elapsed += 1;
+                        if (_callBack) _callBack();
+                    }, 1000);
+                    _state = states.RUNNING;
+                    if (_callBack) _callBack();
+                }
+            },
+
+            pause: function () {
+                ceaseIntervalWithStatus(_interval, states.PAUSED);
+            },
+
+            stop: function () {
+                ceaseIntervalWithStatus(_interval, states.STOPPED);
+                _elapsed = 0;
+            },
+
+            elapsed: function () {
+                return _elapsed;
+            },
+
+            formattedTime: function () {
+                var date = new Date(0);
+                date.setSeconds(_elapsed);
+                return padZero(date.getMinutes()) + ':' + padZero(date.getSeconds());
+            },
+
+            state: function () {
+                return _state;
+            }
+        };
+    }();
+});
--- make.sh
+++ make.sh
@@ -1,2 +1,5 @@
 rm -rfd build
 mkdir build
+cp -r ./src/* ./build
+rm -rfd build/js
+babel src/js -d build/js
